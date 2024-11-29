388fc08cc0b6523af306fa5167fced436da59bd4
maycorolbuche <mayco_rolbuche@hotmail.com>
2024-11-28 21:41:27 -0300

    Ajuste

--- src/modules/Album.vue
+++ src/modules/Album.vue
@@ -13,9 +13,9 @@
   >
     <template v-slot:left>
       <v-table
+        v-if="!loading"
         fixed-header
         hover
-        loading
         class="w-100 h-100"
         :style="{ backgroundColor: module.data.color, color: '#FFF' }"
       >
@@ -63,71 +63,9 @@
           </tr>
         </tbody>
       </v-table>
+
+      <v-progress-linear v-if="loading" color="white" indeterminate />
     </template>
-
-    <!--
-    <l-table
-      v-model="data"
-      :search="search"
-      letter=""
-      :searchable_fields="{
-        track: true,
-        name: true,
-      }"
-      :scroll="scroll"
-      :has_scroll="has_scroll"
-      sort_by="track"
-      :file="$path.db(`/database/album_${module.id_album}.json`)"
-    >
-      <thead>
-        <tr>
-          <th class="text-right">{{ $t("modules.musics.table.track") }}</th>
-          <th class="text-left">{{ $t("modules.musics.table.music_name") }}</th>
-          <th class="text-right">{{ $t("modules.musics.table.duration") }}</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in data.data" :key="item.id_music">
-          <td class="text-right">
-            {{ item.track }}
-          </td>
-          <td>
-            {{ item.name }}
-          </td>
-          <td class="text-right">{{ $datetime.shortTime(item.duration) }}</td>
-          <td>
-            <div class="d-flex justify-end">
-              <l-music-menu-table
-                :id_music="item.id_music"
-                :has_instrumental_music="item.has_instrumental_music"
-              />
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </l-table>
-
-    <v-alert
-      v-if="search && data.filter_count <= 0"
-      type="error"
-      :text="$t('modules.musics.data.not_found')"
-      variant="tonal"
-      border="start"
-      class="ma-2"
-    />
-
-    <template v-slot:footer>
-      <div class="w-100">
-        <div class="text-right">
-          <small>
-            {{ $t("modules.musics.data.records") }}:
-            {{ data.filter_count }}
-          </small>
-        </div>
-      </div>
-    </template>
-    -->
   </l-window>
 </template>
 
@@ -141,7 +79,6 @@ export default {
     LWindow,
     LMusicMenuTable,
   },
-
   computed: {
     module_id() {
       return "album";
@@ -149,7 +86,9 @@ export default {
     module() {
       return this.$modules.get(this.module_id);
     },
+    loading() {
+      return this.$appdata.get("modules.album.loading");
+    },
   },
-  methods: {},
 };
 </script>
