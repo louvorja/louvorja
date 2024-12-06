5c114ab87b558255b3d32d3f656de141fd158c7c
maycorolbuche <mayco_rolbuche@hotmail.com>
2024-12-06 10:58:30 -0300

    Cacheamento e correção de bug

--- src/helpers/Database.js
+++ src/helpers/Database.js
@@ -1,3 +1,4 @@
+import $appdata from "@/helpers/AppData";
 import $alert from "@/helpers/Alert";
 import $path from "@/helpers/Path";
 import $dev from "@/helpers/Dev";
@@ -5,6 +6,16 @@ import $dev from "@/helpers/Dev";
 export default {
   async get(file) {
     try {
+      //Se estiver usando versão On-line, salva em cache os dados
+      const is_online = $appdata.get("is_online");
+      const cache_name = `db-${file}`;
+      const cache = window.sessionStorage.getItem(cache_name);
+
+      if (is_online && cache) {
+        $dev.write(`Lendo BD do cache`, file);
+        return JSON.parse(cache);
+      }
+
       $dev.write("Abrindo BD", file);
       const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
       const response = await fetch(`${$path.db(`/${file}`)}?${date}`, {
@@ -12,8 +23,20 @@ export default {
           "Api-Token": import.meta.env.VITE_API_TOKEN,
         },
       });
+
       if (!response.ok) throw new Error();
-      return await response.json();
+      const data = await response.json();
+
+      if (is_online) {
+        $dev.write("Salvando BD em cache", file);
+        window.sessionStorage.setItem(cache_name, JSON.stringify(data));
+        const expiration = 1000 * 60 * 60 * 24; // 1 dia
+        window.sessionStorage.setItem(
+          `${cache_name}-expiration`,
+          (Date.now() + expiration).toString()
+        );
+      }
+      return data;
     } catch (error) {
       $alert.error({ text: "messages.file_database_not_found", error });
       return null;
