467d5d0c5db5f765baad7d1229673a283e108f81
maycorolbuche <mayco_rolbuche@hotmail.com>
2024-12-05 22:14:40 -0300

    Versão do Programa

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,17 +1,38 @@
 <template>
   <v-footer id="footer-bar" class="pa-0" color="primary">
     <l-player v-if="$media.isMinimized()" location="footer" />
-    <v-row v-else justify="center" no-gutters> </v-row>
+    <v-row v-else no-gutters>
+      <span class="text-caption pa-1">Versão {{ version }}</span>
+    </v-row>
   </v-footer>
 </template>
 
 <script>
+import packageJson from "../../package.json";
+
 import LPlayer from "@/components/Player.vue";
 
 export default {
   name: "FooterLayout",
   components: {
     LPlayer,
+  },
+  data: () => ({
+    db_version: 0,
+  }),
+  computed: {
+    version() {
+      return packageJson.version + "." + this.db_version;
+    },
+  },
+  methods: {
+    async loadDBVersion() {
+      const config = await this.$database.get("config");
+      this.db_version = config.version_number;
+    },
+  },
+  async mounted() {
+    await this.loadDBVersion();
   },
 };
 </script>
