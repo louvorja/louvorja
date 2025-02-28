b98bd1d11a409ace576d0e34fd57b936ce235174
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-02-28 09:22:41 -0300

    Debug

--- src/helpers/Window.js
+++ src/helpers/Window.js
@@ -1,7 +1,13 @@
 export default {
   open(url, target, features) {
     if (url.startsWith("/")) {
-      url = (import.meta.env.BASE_URL ?? "") + url.slice(1);
+      console.log(
+        "ffff",
+        import.meta.env,
+        import.meta.env.BASE_URL,
+        import.meta.env.BASE_URL ?? "/"
+      );
+      url = (import.meta.env.BASE_URL ?? "/") + url.slice(1);
     }
     return window.open(url, target, features);
   },
